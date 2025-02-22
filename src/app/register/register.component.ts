18ce0b70add2b776623064c581ae00b9afdffac6
Walter Iamartino <walteriamartino@gmail.com>
2025-02-22 20:04:49 +0100

    register

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,41 +1,46 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatInputModule } from '@angular/material/input';
-import { MatCardModule } from '@angular/material/card';
-import { ReactiveFormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
-import { AuthService } from '../services/auth.service';
+import { Component } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
+import { MatInputModule } from "@angular/material/input";
+import { MatCardModule } from "@angular/material/card";
+import { ReactiveFormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
+import { AuthService } from "../services/auth.service";
 
 @Component({
-  selector: 'app-register',
-  imports: [MatButtonModule, MatInputModule, MatCardModule, ReactiveFormsModule, CommonModule],
-  templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  selector: "app-register",
+  imports: [
+    MatButtonModule,
+    MatInputModule,
+    MatCardModule,
+    ReactiveFormsModule,
+    CommonModule,
+  ],
+  templateUrl: "./register.component.html",
+  styleUrl: "./register.component.scss",
 })
 export class RegisterComponent {
   registerForm: FormGroup;
 
   constructor(private fb: FormBuilder, private authService: AuthService) {
     this.registerForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]],
-      confirmPassword: ['', [Validators.required, Validators.minLength(6)]]
+      email: ["", [Validators.required, Validators.email]],
+      password: ["", [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ["", [Validators.required, Validators.minLength(6)]],
     });
   }
 
   onSubmit() {
     if (this.registerForm.valid) {
-          this.authService.register(this.registerForm.value).subscribe({
+      this.authService.register(this.registerForm.value).subscribe({
         next: (response) => {
-          localStorage.setItem("token", response.token);
-          debugger;
-          // redirect to home page
+          alert(response.message);
+          // redirect to login page
         },
-        error: (error) => {},
+        error: (error) => {
+          alert(error.message);
+        },
       });
-
-
     }
   }
 }
